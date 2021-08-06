faa1a76f7b5c4573a8b66e70c805f583ca7ca3a1
Daniel Augusto Muller <danielmuller@alunos.utfpr.edu.br>
2021-08-06 16:03:20 -0300

    feat: products with less than 75% of stock view ✨ feat: users and their delivery point city view ✨

--- database/views/view_products_in_stock.sql
+++
@@ -1,7 +0,0 @@
-CREATE VIEW public.view_products_in_stock as
-select p."name",
-    s.quantity
-FROM public.products p
-    join public.stock s on p.id = s.product_id;
-
-select * from public.view_products_in_stock;
