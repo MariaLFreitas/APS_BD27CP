63f487f5b0d93a854304fb365f4ac0a52772bdd5
Daniel Augusto Muller <danielmuller@alunos.utfpr.edu.br>
2021-08-19 18:40:10 -0300

    fiz uns txt com nome de roupa, comida, objetos e eletronicos para usar como produto no mercado

--- database/indexes/IdxFoodCategory.sql
+++ database/indexes/IdxFoodCategory.sql
@@ -1,3 +1,3 @@
--- index com a finalidade de melhorar o desempenho da pesquisa em itens da categoria Comida --
+-- index com a finalidade de melhorar o desempenho da pesquisa em itens
 
-CREATE INDEX idx_food_category ON products(category) WHERE category='Comida';
+CREATE INDEX idx_food_category ON products(category)
